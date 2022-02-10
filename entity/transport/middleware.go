970c14303a42ba05c856b39039cb73674b664110
Karthik <kn@beezlabs.com>
2022-02-10 17:07:41 +0530

    Refactor

--- entity/transport/middleware.go
+++ entity/transport/middleware.go
@@ -11,7 +11,6 @@ import (
 func instrumentingMiddleware(duration metrics.Histogram) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-
 			defer func(begin time.Time) {
 				duration.With("success", fmt.Sprint(err == nil)).Observe(time.Since(begin).Seconds())
 			}(time.Now())
